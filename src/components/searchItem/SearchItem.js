574f50dcd89ba717aa9a3cd3f7522e3f21b2bfbf
Victoria Le <leqvictoria@gmail.com>
2023-03-16 19:54:54 -0400

    siDetails styling

--- src/components/searchItem/SearchItem.js
+++ src/components/searchItem/SearchItem.js
@@ -1,11 +1,17 @@
 import "./searchItem.css"
-import {si1} from "../../assets/images/searchItem/gel_mani.jpg"
+import Si1 from "../../assets/images/searchItem/gel_mani.jpg"
 
 
 const SearchItem = () => {
   return (
     <div className="searchItem">
-      <img className="siImg" alt="" src={si1}/>
+      <img className="siImg" alt="" src={Si1}/>
+      <div className="siDetails">
+        <h1 className="siTitle">Gel Manicure</h1>
+        <span className="siPrice">$42</span>
+        <span className="siDesc">Premium manicure service include cuticle trimming, followed by massage with lotion and hot stones then ended with gel polish.</span>
+        <input type="text" className="siNote" placeholder="Please enter your note, in case you want to request to a specific tech please enter it here"></input>
+      </div>
     </div>
   )
 }

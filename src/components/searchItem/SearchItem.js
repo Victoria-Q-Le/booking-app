4bb1a9b732c8ebe2a3b9f437c053256f0c4c59c5
Victoria Le <leqvictoria@gmail.com>
2023-03-16 19:30:51 -0400

    first sI

--- src/components/searchItem/SearchItem.js
+++ src/components/searchItem/SearchItem.js
@@ -1,9 +1,11 @@
 import "./searchItem.css"
+import {si1} from "../../assets/images/searchItem/gel_mani.jpg"
+
 
 const SearchItem = () => {
   return (
     <div className="searchItem">
-      <img className=""/>
+      <img className="siImg" alt="" src={si1}/>
     </div>
   )
 }

bdb1efece8c1f96a9e8e72715c025f2c9d609747
Victoria Le <leqvictoria@gmail.com>
2023-03-17 19:47:58 -0400

    imgs displayed on service page

--- src/pages/service/Service.js
+++ src/pages/service/Service.js
@@ -3,8 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navBar/Navbar"
 import "./service.css"
+import Img1 from "../../assets/images/searchItem/gel_mani.jpg"
+import Img2 from "../../assets/images/searchItem/dip_design.jpg"
+import Img3 from "../../assets/images/searchItem/gelX.jpg"
+import Img4 from "../../assets/images/searchItem/acrylic.jpg"
 
 const Service = () => {
+  const images = [Img1, Img2, Img3, Img4]
   return (
     <div>
       <Navbar /> 
@@ -20,6 +25,14 @@ const Service = () => {
           <span className="serviceBrand"> We are using natural colors from well known brand such as OPI, Nitro, etc.</span>
 
           <span className="servicePriceHighLight">Bring your friends to get 10% discount for your services</span>
+
+          <div className="serviceImgs">
+           {images.map(image => (
+            <div className="serviceImgWrapper">
+              <img className="serviceImg" alt="" src={image}/>
+            </div>
+           ))}
+          </div>
         </div>
       </div>
     </div>

f59735bf11afd16186ccdfe028a8390dfc69bcd0
Victoria Le <leqvictoria@gmail.com>
2023-03-23 16:51:09 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import HomePage from './pages/home/HomePage';
 import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
+import Landing from './pages/landing/Landing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element = {<HomePage />} />
+        <Route path='/' element = {<Landing />}/>
+        <Route path='/home' element = {<HomePage />} />
         <Route path='/hotels' element = {<List />} />
         <Route path='/hotels/:id' element = {<Hotel />} />
       </Routes>
